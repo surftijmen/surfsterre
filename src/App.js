43d977c15e1dd3b635a5894458993fc49da77aab
Tijmen Meijer <t.g.meijer@student.tudelft.nl>
2022-07-05 12:55:39 +0200

    github pages test

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './recourses/sterre.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+    
+      </header>
+      <p>Hey! My name is Sterre, and this is my personal website. Here you can find information about me, my lifestyle and my sports. Enjoy!</p>
+    </div> 
+  );
+}
+
+export default App;
